401c7e2f3f352a18b71d4f432ad2e13aa162836b
Olivier Toussaint <58084734+OToussaint@users.noreply.github.com>
2023-12-22 11:36:47 +0100

    Add variables sanity checks and error handling

--- backup.sh
+++ backup.sh
@@ -10,7 +10,7 @@ create_daily_backup() {
     # Build the backup command
     cmd="cd \"$CONFIG_DIR\" && find . -name \"*\" -type f -print0 | grep -vz -E \"backups\/(.)+.tar$\" | grep -vz -E \"backups\/(.)+.tar.gz$\" | grep -vz -E \"\#recycle\" | sudo tar --ignore-failed-read -zvcf \"${BACKUP_DIR}/daily-${current_date}-${VERSION}.tar.gz\" --null -T -"
 
-    eval "$cmd"  # Execute the backup command
+    eval "$cmd" || { echo "Failed to create a daily backup" >&2; exit 1; }
 }
 
 # Read config file
@@ -26,8 +26,28 @@ fi
 VENV_DIR=$(echo "$config" | jq -r '.VENV_DIR // "/srv/homeassistant"')
 CONFIG_DIR=$(echo "$config" | jq -r '.CONFIG_DIR // "/home/homeassistant/.homeassistant"')
 HA_USER=$(echo "$config" | jq -r '.HA_USER // "homeassistant"')
-BACKUP_DIR=$(echo "$config" | jq -r '.BACKUP_DIR // "homeassistant"')
+BACKUP_DIR=$(echo "$config" | jq -r '.BACKUP_DIR // "/home/homeassistant/.homeassistant/backups"')
 VERSION=$(sudo -u "${HA_USER}" -H -s /bin/bash -c "source ${VENV_DIR}/bin/activate && hass --version")
+
+# Validate VENV_DIR, CONFIG_DIR, and BACKUP_DIR directories
+if [[ ! -d "${VENV_DIR}" || ! -d "${CONFIG_DIR}" || ! -d "${BACKUP_DIR}" ]]; then
+    echo "One or more directories specified in the configuration do not exist."
+    exit 1
+fi
+
+# Check if HA_USER is a valid user
+getent passwd "${HA_USER}" > /dev/null
+if [ $? -ne 0 ]; then
+    echo "The user '${HA_USER}' specified in the configuration is invalid."
+    exit 1
+fi
+
+# Validate VERSION command
+sudo -u "${HA_USER}" -H -s /bin/bash -c "source ${VENV_DIR}/bin/activate && hass --version" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Failed to get Home Assistant version. Please check the configuration and Home Assistant installation."
+    exit 1
+fi
 
 # Manage weekly backup function
 manage_weekly_backup() {
@@ -35,16 +55,11 @@ manage_weekly_backup() {
     current_week=$(date "+%Y-w%U")
     last_daily_backup=$(ls -1t "$BACKUP_DIR/daily-"* | head -n 1)
 
-    # Create a weekly backup if there's a recent daily backup
-    if [ -n "$last_daily_backup" ]; then
-        cp -p "$BACKUP_DIR/$last_daily_backup" "$BACKUP_DIR/weekly-${current_week}.tar.gz" 2>/dev/null
-    fi
-
+    # Create a weekly backup using the most recent daily backup if it exists
     latest_daily_backup=$(find "${BACKUP_DIR}" -maxdepth 1 -type f -name "daily-*.tar.gz" -mtime -7 | sort | tail -1)
 
-    # Create a weekly backup with the latest daily backup content
     if [ -n "${latest_daily_backup}" ]; then
-        cp -p "${latest_daily_backup}" "${BACKUP_DIR}/weekly-${current_week}-${VERSION}.tar.gz" 2>/dev/null
+        cp -p "${latest_daily_backup}" "${BACKUP_DIR}/weekly-${current_week}.tar.gz" 2>/dev/null || { echo "Failed to copy the latest daily backup for the weekly backup" >&2; exit 1; }
     fi
 }
 
@@ -56,24 +71,23 @@ manage_monthly_backup() {
 
     # Create a monthly backup on the 1st day of the month
     if [ "$(date '+%d')" -eq "01" ]; then
-        cp -p "$BACKUP_DIR/$last_daily_backup" "$BACKUP_DIR/monthly-${last_month}-${VERSION}.tar.gz" 2>/dev/null
+        cp -p "$BACKUP_DIR/$last_daily_backup" "$BACKUP_DIR/monthly-${last_month}-${VERSION}.tar.gz" 2>/dev/null || { echo "Failed to copy the latest daily backup for the monthly backup" >&2; exit 1; }
     fi
 }
-
 # Everyday: create a daily backup
-create_daily_backup
+create_daily_backup || exit 1
 
 # Every week, on Monday (day 1): create a weekly backup
 if [ "$(date +%u)" -eq 1 ]; then
-    manage_weekly_backup
+    manage_weekly_backup || exit 1
 fi
 
 # Every 1st of the month: create a monthly backup
 if [ "$(date +%d)" = "01" ]; then
-    manage_monthly_backup
+    manage_monthly_backup || exit 1
 fi
 
 # Remove old backups (keep 7 daily, 4 weekly, 12 monthly)
-find "${BACKUP_DIR}" -type f -name "daily-*.tar.gz" -mtime +7 -delete
-find "${BACKUP_DIR}" -type f -name "weekly-*.tar.gz" -mtime +28 -delete
-find "${BACKUP_DIR}" -type f -name "monthly-*.tar.gz" -mtime +365 -delete
+find "${BACKUP_DIR}" -type f -name "daily-*.tar.gz" -mtime +7 -delete || { echo "Failed to delete old daily backups" >&2; exit 1; }
+find "${BACKUP_DIR}" -type f -name "weekly-*.tar.gz" -mtime +28 -delete || { echo "Failed to delete old weekly backups" >&2; exit 1; }
+find "${BACKUP_DIR}" -type f -name "monthly-*.tar.gz" -mtime +365 -delete || { echo "Failed to delete old monthly backups" >&2; exit 1; }
